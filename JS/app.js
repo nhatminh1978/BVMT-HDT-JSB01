2c1b6719d3851d7af5f298fd385f31d69e28179d
nhatminh1978 <38359524+nhatminh1978@users.noreply.github.com>
2020-11-26 19:36:45 +0700

    code đây nhé a hiếu

---
+++ JS/app.js
@@ -0,0 +1,24 @@
+var input = document.getElementById("myInput");
+  function myFunction() {
+      var filter, ul, li, a, i;
+      filter = input.value.toUpperCase();
+      ul = document.getElementById("myUL");
+      li = ul.getElementsByTagName("li");
+      if (!filter) {
+        ul.style.display = "none";
+      }else{
+        for (i = 0; i < li.length; i++) {
+            a = li[i].getElementsByTagName("a")[0];
+            if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                ul.style.display = "block";
+                li[i].style.display = "";
+            } else {
+                li[i].style.display = "none"; 
+            }
+        }
+    }
+}
+input.addEventListener("keyup", myFunction);
+let search = document.getElementById("search-button").addEventListener('click',function(){
+    input.style.display="block";
+})
