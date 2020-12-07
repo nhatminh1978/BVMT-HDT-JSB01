c8c864b9a3688b0ebd074f863fd9fa3af4a849a3
nhatminh1978 <38359524+nhatminh1978@users.noreply.github.com>
2020-12-07 19:45:51 +0700

    code nhé a!

--- JS/app.js
+++ JS/app.js
@@ -22,3 +22,31 @@ input.addEventListener("keyup", myFunction);
 let search = document.getElementById("search-button").addEventListener('click',function(){
     input.style.display="block";
 })
+let today = new Date();
+let current_day = today.getDay();
+let day_name;
+switch (current_day) {
+    case 0:
+        day_name = "Chủ nhật";
+        break;
+    case 1:
+        day_name = "Thứ hai";
+        break;
+    case 2:
+        day_name = "Thứ ba";
+        break;
+    case 3:
+        day_name = "Thứ tư";
+        break;
+    case 4:
+        day_name = "Thứ năm";
+        break;
+    case 5:
+        day_name = "Thứ sau";
+        break;
+    case 6:
+        day_name = "Thứ bảy";
+        break;
+    }
+let date = day_name + ", " + today.getDate() + "/" + today.getMonth() + "/" + today.getFullYear();
+document.getElementById("date").innerHTML = date;
