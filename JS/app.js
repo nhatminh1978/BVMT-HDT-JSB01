9fe0763721a489b6310c5c445561760c6a20a640
nhatminh1978 <38359524+nhatminh1978@users.noreply.github.com>
2020-12-14 22:55:59 +0700

    new code

--- JS/app.js
+++ JS/app.js
@@ -1,26 +1,28 @@
 var input = document.getElementById("myInput");
-  function myFunction() {
-      var filter, ul, li, a, i;
-      filter = input.value.toUpperCase();
-      ul = document.getElementById("myUL");
-      li = ul.getElementsByTagName("li");
-      if (!filter) {
+
+function myFunction() {
+    let filter, ul, li, a, i;
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    if (!filter) {
         ul.style.display = "none";
-      }else{
+    } else {
         for (i = 0; i < li.length; i++) {
             a = li[i].getElementsByTagName("a")[0];
             if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
                 ul.style.display = "block";
                 li[i].style.display = "";
             } else {
-                li[i].style.display = "none"; 
+                li[i].style.display = "none";
             }
         }
     }
 }
-input.addEventListener("keyup", myFunction);
-let search = document.getElementById("search-button").addEventListener('click',function(){
-    input.style.display="block";
+input.addEventListener("keyup", myFunction());
+
+let search = document.getElementById("search-button").addEventListener('click', function () {
+    input.style.display = "block";
 })
 let today = new Date();
 let current_day = today.getDay();
@@ -47,11 +49,19 @@ switch (current_day) {
     case 6:
         day_name = "Thứ bảy";
         break;
-    }
+}
 let date = day_name + ", " + today.getDate() + "/" + today.getMonth() + "/" + today.getFullYear();
 document.getElementById("date").innerHTML = date;
-$(function(){
+$(function () {
     $('#top-up').click(function () {
-        $('body,html').animate({scrollTop: 0}, 600);
+        $('body,html').animate({ scrollTop: 0 }, 600);
     });
 });
+const old = document.querySelector(".old");
+console.log(old);
+let left = document.getElementById('arrow-left').addEventListener("click", function () {
+    left.style.display = "block";
+})
+let right = document.getElementById('fa-arrow-right').addEventListener("click", function () {
+    old.style.display = "none";
+})
